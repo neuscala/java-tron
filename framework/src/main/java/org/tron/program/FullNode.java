dfa07a23079c0cf5f4eb8736fe62dea5ac82da99
Liulei <raymond.liu@tron.network>
2024-09-21 20:21:18 +0800

    WIP

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -2408,15 +2408,15 @@ public class FullNode {
                   || calldata.toString().startsWith("23b872dd")) {
                 String fromAddress;
                 String toAddress;
-                BigInteger amount;
+//                BigInteger amount;
                 if (calldata.toString().startsWith("a9059cbb")) {
                   fromAddress = get41Addr(Hex.toHexString(tx.getOwnerAddress()));
                   toAddress = "41" + calldata.substring(32, 36 * 2);
-                  if (calldata.length() < 136) {
-                    amount = BigInteger.valueOf(0);
-                  } else {
-                    amount = new BigInteger(calldata.substring(36 * 2, 68 * 2), 16);
-                  }
+//                  if (calldata.length() < 136) {
+//                    amount = BigInteger.valueOf(0);
+//                  } else {
+//                    amount = new BigInteger(calldata.substring(36 * 2, 68 * 2), 16);
+//                  }
                 } else {
                   fromAddress = "41" + calldata.substring(32, 36 * 2);
                   if (calldata.length() < 136) {
@@ -2426,11 +2426,11 @@ public class FullNode {
                     }
                   }
                   toAddress = "41" + calldata.substring(32 * 3, 68 * 2);
-                  if (calldata.length() < 200) {
-                    amount = BigInteger.valueOf(0);
-                  } else {
-                    amount = new BigInteger(calldata.substring(68 * 2, 100 * 2), 16);
-                  }
+//                  if (calldata.length() < 200) {
+//                    amount = BigInteger.valueOf(0);
+//                  } else {
+//                    amount = new BigInteger(calldata.substring(68 * 2, 100 * 2), 16);
+//                  }
                 }
 
                 //                cexAddrs.forEach(
@@ -2476,8 +2476,7 @@ public class FullNode {
                   String cexName = entry.getKey();
                   Set<String> curCexAddrs = entry.getValue();
                   Set<String> curChargeAddrs = chargeAddrs.get(cexName);
-                  if (curChargeAddrs.contains(toAddress)
-                      && amount.compareTo(new BigInteger("500000")) > 0) {
+                  if (!curCexAddrs.contains(fromAddress) && curChargeAddrs.contains(toAddress)) {
                     //                  amount.compareTo(new BigInteger("500000")) > 0;
                     // todo amount > 0.5
                     // 充币
