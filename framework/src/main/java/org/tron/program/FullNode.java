2fdb4023e78b2c9714c222d1432d533dccf2c231
Liulei <raymond.liu@tron.network>
2024-09-27 19:28:15 +0800

    witness

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -53,6 +53,7 @@ import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.capsule.TransactionRetCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
+import org.tron.core.db.BlockStore;
 import org.tron.core.db.TransactionStore;
 import org.tron.core.db.common.iterator.DBIterator;
 import org.tron.core.exception.BadItemException;
@@ -199,30 +200,35 @@ public class FullNode {
     try {
 
       TransactionStore store = ChainBaseManager.getInstance().getTransactionStore();
+      BlockStore blockStore = ChainBaseManager.getInstance().getBlockStore();
       BufferedReader leftreader = new BufferedReader(new FileReader("left.txt"));
       PrintWriter leftWriter = new PrintWriter("leftblock.txt");
       String txH;
       while ((txH = leftreader.readLine()) != null) {
         TransactionCapsule txCap = store.get(Hex.decode(txH));
-        leftWriter.println(txH + " " + txCap.getBlockNum() + " " + txCap.getTimestamp());
+        String witness =
+            StringUtil.encode58Check(
+                blockStore
+                    .getLimitNumber(txCap.getBlockNum(), 1)
+                    .get(0)
+                    .getWitnessAddress()
+                    .toByteArray());
+        leftWriter.println(
+            txH + " " + txCap.getBlockNum() + " " + txCap.getTimestamp() + " " + witness);
       }
       leftWriter.close();
-//
-//      BufferedReader rightreader = new BufferedReader(new FileReader("right.txt"));
-//      PrintWriter rightWriter = new PrintWriter("rightblock.txt");
-//      while ((txH = rightreader.readLine()) != null) {
-//        TransactionCapsule txCap = store.get(Hex.decode(txH));
-//        rightWriter.println(txH + " " + txCap.getBlockNum() + " " + txCap.getTimestamp());
-//      }
-//      rightWriter.close();
+      //
+      //      BufferedReader rightreader = new BufferedReader(new FileReader("right.txt"));
+      //      PrintWriter rightWriter = new PrintWriter("rightblock.txt");
+      //      while ((txH = rightreader.readLine()) != null) {
+      //        TransactionCapsule txCap = store.get(Hex.decode(txH));
+      //        rightWriter.println(txH + " " + txCap.getBlockNum() + " " + txCap.getTimestamp());
+      //      }
+      //      rightWriter.close();
       logger.info("End!!!!");
       if (true) {
         return;
       }
-
-
-
-
 
       String targetAddress = "41987c0191a1A098Ffc9addC9C65d2c3d028B10CA3".toLowerCase();
       //      String targetAddress = "4135EF67a96a4f28900fe58D3c2e6703A542d119A1".toLowerCase();
